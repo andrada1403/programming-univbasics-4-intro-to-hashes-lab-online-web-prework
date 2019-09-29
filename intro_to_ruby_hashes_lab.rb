67210820ae2bb9a97fdb53d3655f93970aba872a
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-09-29 16:34:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   key=name
-  hash={name
+  hash={key=>value}
 end
 
 def update_counting_hash(hash, key)
